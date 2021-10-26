effafe22f2e08b041f91fafbf13f95cf2249000a
Keita Okada <okeita.0805@gmail.com>
2021-10-27 02:08:45 +0900

    prettierとeslintの設定

--- resources/js/components/Example.tsx
+++ resources/js/components/Example.tsx
@@ -1,20 +1,23 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Example = () => (
-    <div className="container">
-        <div className="row justify-content-center">
-            <div className="col-md-8">
-                <div className="card">
-                    <div className="card-header">Example Component</div>
-
-                    <div className="card-body">I'm an example component!</div>
-                </div>
-            </div>
+  <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-header">Example Component</div>
+          <Button variant="contained" color="primary">
+            Contained
+          </Button>
+          <div className="card-body">I'm an example component!</div>
         </div>
+      </div>
     </div>
-)
+  </div>
+);
 
 if (document.getElementById('app')) {
-    ReactDOM.render(<Example />, document.getElementById('app'));
+  ReactDOM.render(<Example />, document.getElementById('app'));
 }
