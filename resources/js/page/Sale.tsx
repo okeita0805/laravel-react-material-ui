56f5be87628d45d2ea312f3521d6fd6dc28acf2b
Keita Okada <okeita.0805@gmail.com>
2021-12-05 22:11:03 +0900

    routerの追加とトップページのレイアウト修正

---
+++ resources/js/page/Sale.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { Grid, Paper, styled } from "@material-ui/core";
+import SaleHistoryTable from "../components/Sale/Table";
+import SaleCalculate from "../components/Sale/Calculate";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+  })
+);
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export default function Top() {
+  const classes = useStyles();
+
+  const [isHistory, setIsHistory] = useState(true);
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Item onClick={() => setIsHistory(true)}>注文履歴</Item>
+      </Grid>
+      <Grid item xs={6}>
+        <Item onClick={() => setIsHistory(false)}>売上修正</Item>
+      </Grid>
+      <Grid item xs={12}>
+        {isHistory ? <SaleHistoryTable /> : <SaleCalculate />}
+      </Grid>
+    </Grid>
+  );
+}
