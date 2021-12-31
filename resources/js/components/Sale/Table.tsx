1e80716b15557708b76d387c111d2883b1a9195b
Keita Okada <okeita.0805@gmail.com>
2021-12-31 22:22:30 +0900

    詳細の作成

--- resources/js/components/Sale/Table.tsx
+++ resources/js/components/Sale/Table.tsx
@@ -11,6 +11,8 @@ import {
   Button,
   createStyles,
   Dialog,
+  Divider,
+  Grid,
   IconButton,
   styled,
   Theme,
@@ -38,6 +40,15 @@ const useStyles = makeStyles({
 
 const BoldTableCell = styled(TableCell)(() => ({
   fontWeight: "bold",
+}));
+
+const DetailTitle = styled("span")(() => ({
+  fontWeight: "bold",
+  float: "right",
+}));
+
+const DetailTitleBody = styled("span")(() => ({
+  marginLeft: "8px",
 }));
 
 const styles = (theme: Theme) =>
@@ -149,6 +160,7 @@ export default function SaleHistoryTable() {
         </Table>
       </TableContainer>
       <Dialog
+        maxWidth={"md"}
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}>
@@ -156,21 +168,89 @@ export default function SaleHistoryTable() {
           注文番号: {orderHistory.orderNumber}
         </DialogTitle>
         <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-            auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-            cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-            dui. Donec ullamcorper nulla non metus auctor fringilla.
-          </Typography>
+          <Grid container style={{ marginBottom: "12px", fontSize: "16px" }}>
+            <Grid item xs={6}>
+              <Grid container>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitle>提供方法:</DetailTitle>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitleBody>
+                    {orderHistory.expeditionType}
+                  </DetailTitleBody>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitle>サービス名:</DetailTitle>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitleBody>{orderHistory.serviceType}</DetailTitleBody>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitle>氏名:</DetailTitle>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitleBody>{orderHistory.name}</DetailTitleBody>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitle>お届け先住所:</DetailTitle>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitleBody>{orderHistory.address}</DetailTitleBody>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitle>電話番号:</DetailTitle>
+                </Grid>
+                <Grid item xs={6}>
+                  <DetailTitleBody>{orderHistory.phone}</DetailTitleBody>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={6}>
+              <Grid container>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitle>支払い方式:</DetailTitle>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitleBody>
+                    {orderHistory.paymentMethod}
+                  </DetailTitleBody>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitle>注文日時:</DetailTitle>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitleBody> {orderHistory.orderedAt}</DetailTitleBody>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitle>お渡し・お届け時間:</DetailTitle>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitleBody>{orderHistory.deliveredAt}</DetailTitleBody>
+                </Grid>
+                <Grid item xs={6} style={{ marginBottom: "12px" }}>
+                  <DetailTitle>使い捨て容器:</DetailTitle>
+                </Grid>
+                <Grid item xs={6}>
+                  <DetailTitleBody>
+                    {orderHistory.isDisposable ? "使用する" : "使用しない"}
+                  </DetailTitleBody>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          注文一覧
+          <Divider />
+          <Divider />
+          <Grid container>
+            <Grid item xs={6}>
+              注文に関するご要望:
+            </Grid>
+            <Grid item xs={6}>
+              <Grid item>小計 (内税)</Grid>
+              <Grid item>注文割引額</Grid>
+              <Grid item>合計支払額</Grid>
+            </Grid>
+          </Grid>
         </DialogContent>
       </Dialog>
     </>
